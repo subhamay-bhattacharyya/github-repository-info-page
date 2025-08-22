1e4e594cd708e821bcf952b6200976dc1b78b05d
Subhamay Bhattacharyya <142895397+bsubhamay@users.noreply.github.com>
2025-08-22 18:50:51 +0000

    fix: Update .gitignore to include Terraform lock file and clean up locals.tf

--- tf/locals.tf
+++ tf/locals.tf
@@ -4,7 +4,4 @@ locals {
   cloudformation-repos    = jsondecode(file("${path.module}/all-repositories/cloudformation-repos.json"))
   terraform-repos         = jsondecode(file("${path.module}/all-repositories/terraform-repos.json"))
   currently-working-repos = jsondecode(file("${path.module}/all-repositories/currently-working-repos.json"))
-
-  flat_cloudformation_repos = flatten([for repo in local.cloudformation-repos : repo])
-
 }
--- tf/providers.tf
+++ tf/providers.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     github = {
-      source  = "integrations/github"
-      version = "6.6.0"
+      source           = "integrations/github"
+      version          = "6.6.0"
     }
     time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
+      source           = "hashicorp/time"
+      version          = "0.9.1"
     }
   }
 }
